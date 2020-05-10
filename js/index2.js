dd65596fdfcbfb68912c80d1215b30cc7e2c9423
bobbizhen <zhm12zz@sina.com>
2020-05-10 20:44:40 +0800

    myworks

--- js/index2.js
+++ js/index2.js
@@ -13,7 +13,8 @@ var vm = new Vue({
 	data:{
 		works:[
 			{
-				pic:'images/bank_客户分析系统.jpg',
+				pic:'images/bank_客户分析系统-2.jpg',
+				pic2:'images/bank_客户分析系统.jpg',
 				video:'',
 				title:'客户分析系统',
 				classify:'银行金融',
@@ -21,83 +22,95 @@ var vm = new Vue({
 			},
 			{
 				pic:'images/default.jpg',
+				pic2:'images/default.jpg',
 				video:'images/图标动画.mp4',
 				title:'图标动效',
 				classify:'建筑地产',
 				time:'交互动效'
 			},
 			{
-				pic:'images/地产_供应商协同平台.jpg',
+				pic:'images/地产_供应商协同平台-2.jpg',
+				pic2:'images/地产_供应商协同平台.jpg',
 				video:'',
 				title:'供应商协同平台',
 				classify:'建筑地产',
 				time:'UI 前端'
 			},
 			{
-				pic:'images/政企云.jpg',
+				pic:'images/政企云-2.jpg',
+				pic2:'images/政企云.jpg',
 				video:'',
 				title:'中国移动政企云',
 				classify:'通信政府',
 				time:'UI'
 			},
 			{
-				pic:'images/SAP_界面优化.jpg',
+				pic:'images/SAP_界面优化-2.jpg',
+				pic2:'images/SAP_界面优化.jpg',
 				video:'',
 				title:'SAP_界面优化',
 				classify:'建筑地产',
 				time:'UI 前端'
 			},
 			{
-				pic:'images/中国建投-股权投资产业经营.jpg',
+				pic:'images/中国建投-股权投资产业经营-2.jpg',
+				pic2:'images/中国建投-股权投资产业经营.jpg',
 				video:'',
 				title:'股权投资产业经营',
 				classify:'建筑地产',
 				time:'UI'
 			},
 			{
-				pic:'images/移动端-物业app.jpg',
+				pic:'images/移动端-物业app-2.jpg',
+				pic2:'images/移动端-物业app.jpg',
 				video:'',
 				title:'物业相关app',
 				classify:'建筑地产',
 				time:'UI'
 			},
 			{
-				pic:'images/标签拖拽删除.jpg',
+				pic:'images/标签拖拽删除-2.jpg',
+				pic2:'images/标签拖拽删除.jpg',
 				video:'images/标签拖拽删除.mp4',
 				title:'标签拖拽动效',
 				classify:'建筑地产',
 				time:'交互动效'
 			},
 			{
-				pic:'images/移动端-客户app.jpg',
+				pic:'images/移动端-客户app-2.jpg',
+				pic2:'images/移动端-客户app.jpg',
 				video:'',
 				title:'地产客户app',
 				classify:'建筑地产',
 				time:'UI'
 			},
 			{
-				pic:'images/地产-移动办公app.jpg',
+				pic:'images/地产-移动办公app-2.jpg',
+				pic2:'images/地产-移动办公app.jpg',
 				video:'',
 				title:'移动办公app',
 				classify:'建筑地产',
 				time:'UI'
 			},
 			{
-				pic:'images/联动-上移-缩小透明-模糊.jpg',
+				pic:'images/联动-上移-缩小透明-模糊-2.jpg',
+				pic2:'images/联动-上移-缩小透明-模糊.jpg',
 				video:'images/联动-上移-缩小透明-模糊.mp4',
 				title:'联动上移动效',
 				classify:'建筑地产',
 				time:'交互动效'
 			},
 			{
-				pic:'images/picview01.jpg',
+				pic:'images/picview01-2.jpg',
+				pic2:'images/picview01.jpg',
 				video:'',
 				title:'H5页面',
 				classify:'银行金融',
 				time:'UI 前端'
 			},
 			{
-				pic:'images/picview03.jpg',
+				pic:'images/picview03-2.jpg',
+				pic2:'images/picview03.jpg',
 				video:'',
 				title:'智链融资贷款',
 				classify:'金融',
@@ -105,41 +118,47 @@ var vm = new Vue({
 			},
 			{
 				pic:'images/default.jpg',
+				pic2:'images/default.jpg',
 				video:'images/装车系统演示flash.mp4',
 				title:'装车系统演示flash',
 				classify:'能源',
 				time:'交互动效'
 			},
 			{
-				pic:'images/picview04.jpg',
+				pic:'images/picview04-2.jpg',
+				pic2:'images/picview04.jpg',
 				video:'',
 				title:'党建学习平台',
 				classify:'党建政府',
 				time:'UI 前端'
 			},
 			{
-				pic:'images/picview06.jpg',
+				pic:'images/picview06-2.jpg',
+				pic2:'images/picview06.jpg',
 				video:'',
 				title:'华润供应商·万科电商',
 				classify:'建筑地产',
 				time:'UI 前端'
 			},
 			{
-				pic:'images/picview05.jpg',
+				pic:'images/picview05-2.jpg',
+				pic2:'images/picview05.jpg',
 				video:'',
 				title:'工业云·兴业云·城市云',
 				classify:'政府国企',
 				time:'UI 前端'
 			},
 			{
-				pic:'images/picview08.jpg',
+				pic:'images/picview08-2.jpg',
+				pic2:'images/picview08.jpg',
 				video:'',
 				title:'融创供应商',
 				classify:'建筑地产',
 				time:'UI 前端'
 			},
 			{
-				pic:'images/picview09.jpg',
+				pic:'images/picview09-2.jpg',
+				pic2:'images/picview09.jpg',
 				video:'',
 				title:'大屏显示',
 				classify:'政府机构',
@@ -147,6 +166,7 @@ var vm = new Vue({
 			},
 			{
 				pic:'images/default.jpg',
+				pic2:'images/default.jpg',
 				video:'images/联动香水（page、位移、缩放）.mp4',
 				title:'联动动效',
 				classify:'建筑地产',
@@ -154,7 +174,8 @@ var vm = new Vue({
 			},
 		],
 		detail:{
-				pic:'images/联动-上移-缩小透明-模糊.jpg',
+				pic:'images/联动-上移-缩小透明-模糊-2.jpg',
+				pic2:'images/联动-上移-缩小透明-模糊.jpg',
 				video:'images/联动-上移-缩小透明-模糊.mp4',
 				title:'联动上移动效',
 				classify:'建筑地产',
